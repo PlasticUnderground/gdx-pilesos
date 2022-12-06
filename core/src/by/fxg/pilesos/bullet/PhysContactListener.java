5cf595c4d7f4f6660dd58fb23077533f24add1b1
fxgaming <fxgsteam@mail.ru>
2022-12-06 10:48:46 +0300

    Fix instantiation

--- core/src/by/fxg/pilesos/bullet/PhysContactListener.java
+++ core/src/by/fxg/pilesos/bullet/PhysContactListener.java
@@ -9,7 +9,7 @@ public class PhysContactListener extends ContactListener {
 	private static PhysContactListener instance;
 	
 	/** Creates main instance of PhysContactListener **/
-	public static void create() {  if (instance != null) instance = new PhysContactListener(); }
+	public static void create() {  if (instance == null) instance = new PhysContactListener(); }
 	
 	/** Enables main instance of PhysContactListener if was disabled before **/
 	public static void reEnable() { if (instance != null) instance.enable(); }
@@ -61,7 +61,7 @@ public class PhysContactListener extends ContactListener {
 	
 	//calls per single shape face contact creation (maybe)
 	public boolean onContactAdded(btManifoldPoint manifoldPoint, btCollisionObject object0, int partID0, int index0, btCollisionObject object1, int partID1, int index1) {
-		// TODO FIXME
+		// TODO [Physics] add methods with indexes args
 		// Indexes are not used in sublisteners. Main point of this decision is that i don't know what are they do
 		//  and where they're can be used. If indexes are uservalues, its right decision that they're not used because
 		//  user values are usually using as parent objects instead of indexes.
